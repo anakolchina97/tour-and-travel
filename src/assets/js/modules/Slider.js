fa72ba84f3e0b4e157d4633e3732a815cd474215
Annie <anniehartmann97@gmail.com>
2023-01-26 00:13:44 +0300

    Tablet, mobile

--- src/assets/js/modules/Slider.js
+++ src/assets/js/modules/Slider.js
@@ -10,12 +10,15 @@ class Slider {
 			breakpoints: {
 				320: {
 					slidesPerView: 1,
+					spaceBetween: 30,
 				},
 				768: {
 					slidesPerView: 2,
+					spaceBetween: 30,
 				},
 				1200: {
 					slidesPerView: 3,
+					spaceBetween: 30,
 				},
 				1400: {
 					slidesPerView: 4,
